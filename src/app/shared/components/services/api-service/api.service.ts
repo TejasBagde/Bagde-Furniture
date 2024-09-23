f73e775fcaaa8b6080990d11e66e9a50a3920dd7
TejasBagde <tejasbagde888@gmail.com>
2024-09-24 01:16:46 +0530

    discount home ui

--- src/app/shared/components/services/api-service/api.service.ts
+++ src/app/shared/components/services/api-service/api.service.ts
@@ -9,12 +9,12 @@ import { GenericResponse, PincodeResponse } from '../../../../order/models/order
 export class ApiService {
 
   homeBoxImages: SingleProduct[] = [
-    {id: 1, name: 'Henry 3 Seater Sofa', description: 'this is test for json1', image: 'assets/sofas/1.webp', showHeartIcon: false, price: 20000, material: 'leather', discount: 30},
-    {id: 2, name: 'Sofa2', description: 'this is test for json2', image: 'assets/sofas/2.jpg', showHeartIcon: false, price: 20000, material: 'leather', discount: 30},
-    {id: 3, name: 'Sofa3', description: 'this is test for json3', image: 'assets/sofas/3.jpg', showHeartIcon: false, price: 20000, material: 'leather', discount: 30},
-    {id: 4, name: 'Sofa4', description: 'this is test for json4', image: 'assets/sofas/4.jpg', showHeartIcon: false, price: 20000, material: 'leather', discount: 30},
-    {id: 5, name: 'Sofa5', description: 'this is test for json5', image: 'assets/sofas/5.webp', showHeartIcon: false, price: 20000, material: 'leather', discount: 30},
-    {id: 6, name: 'Sofa6', description: 'this is test for json6', image: 'assets/sofas/6.webp', showHeartIcon: false, price: 20000, material: 'leather', discount: 30},
+    {id: 1, name: 'Henry 3 Seater Sofa', description: 'this is test for json1', image: 'assets/sofas/1.webp', showHeartIcon: false, price: 200, material: 'leather', discount: 30},
+    {id: 2, name: 'Sofa2', description: 'this is test for json2', image: 'assets/sofas/2.jpg', showHeartIcon: false, price: 100, material: 'leather', discount: 30},
+    {id: 3, name: 'Sofa3', description: 'this is test for json3', image: 'assets/sofas/3.jpg', showHeartIcon: false, price: 450, material: 'leather', discount: 30},
+    {id: 4, name: 'Sofa4', description: 'this is test for json4', image: 'assets/sofas/4.jpg', showHeartIcon: false, price: 200, material: 'leather', discount: 30},
+    {id: 5, name: 'Sofa5', description: 'this is test for json5', image: 'assets/sofas/5.webp', showHeartIcon: false, price: 150, material: 'leather', discount: 30},
+    {id: 6, name: 'Sofa6', description: 'this is test for json6', image: 'assets/sofas/6.webp', showHeartIcon: false, price: 750, material: 'leather', discount: 30},
   ];
 
   constructor(private http: HttpClient) { }
