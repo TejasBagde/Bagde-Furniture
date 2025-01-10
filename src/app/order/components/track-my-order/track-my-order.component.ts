7c2580e00f73cc0f28c12f081a483bde341635f6
Tejas Bagde <tejas.bagde@onerooftech.com>
2025-01-10 18:58:40 +0530

    track order ui done

--- src/app/order/components/track-my-order/track-my-order.component.ts
+++ src/app/order/components/track-my-order/track-my-order.component.ts
@@ -1,12 +1,44 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-track-my-order',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './track-my-order.component.html',
   styleUrl: './track-my-order.component.css'
 })
 export class TrackMyOrderComponent {
+
+  currentStep: number = 1;
+  totalSteps: number = 5; // Total number of steps in the wizard
+
+  // You can store the form data here if needed
+  formData: any = {
+    step1Data: '',
+    step2Data: '',
+    step3Data: ''
+  };
+
+  goToNextStep() {
+    if (this.currentStep < this.totalSteps) {
+      this.currentStep++;
+    }
+  }
+
+  goToPreviousStep() {
+    if (this.currentStep > 1) {
+      this.currentStep--;
+    }
+  }
+
+  isStepActive(step: number): boolean {
+    return this.currentStep === step;
+  }
+
+  submitWizard() {
+    console.log('Form Data:', this.formData);
+    alert('Wizard Completed!');
+  }
 
 }
